d86a6b89ba0652b4bc440536e430ce3a8a450471
thattyson <thattyson@gmail.com>
2015-06-25 18:57:45 -0700

    Update project settings

--- TTWindowManagerSample/Pods/Target Support Files/Pods-TTWindowManagerSample-TTWindowManager/Pods-TTWindowManagerSample-TTWindowManager-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "TTWindow.h"
-#import "TTWindowManager.h"
-
-FOUNDATION_EXPORT double TTWindowManagerVersionNumber;
-FOUNDATION_EXPORT const unsigned char TTWindowManagerVersionString[];
-
